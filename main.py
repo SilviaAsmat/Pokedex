072e7c4141fb97f438062136944fa7d92d0b32ee
Silvia Asmat <silvia.asmat@g.austincc.edu>
2022-05-27 12:56:51 -0500

    Added search function by name and ID

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # importing csv module
 import csv
+import os
+import glob
+
+
 
 # csv file name
 filename = "pokemon.csv"
@@ -24,12 +28,32 @@ with open(filename, 'r') as csvfile:
 	print("Total no. of rows: %d"%(csvreader.line_num))
 
 # printing the field names
-print('Field names are:' + ', '.join(field for field in fields))
+#print('Field names are:' + ', '.join(field for field in fields))
 
-# printing first 5 rows
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-	# parsing each column of a row
-	for col in row:
-		print("%10s" %col,' '),
-	print('\n')
+print("Pokedex\n")
+print("Welcome Pokemon Lovers\n")
+print("Search for a pokemon\n")
+letterChoice = input("<A>Search by pokemon name\n<B>Search by pokemon ID\n(select A or B)\n")
+letterChoice.upper()
+
+if(letterChoice == "A"):
+    print("Enter the name of the pokemon")
+    name = input()
+    name.lower().strip()
+    dt = filename[:].where(filename['pokemon']==name)
+    st = dt[dt['id'].notnull()]    
+    idx = dt.index[dt['pokemon'] == name]
+    
+    for i in st.columns:
+      print(i, " : ", st[i][idx[0]])
+    
+    
+elif(letterChoice == "B"):
+    print("Enter the ID of the pokemon")
+    ID = int(input())
+    tt = filename[:].where(filename['id'] == ID)
+    idx1 = tt.index[tt['id'] == ID]
+    qt = tt[tt['id'].notnull()]
+    for i in qt.columns:
+        print(i," : ",qt[i][idx1[0]])
+
